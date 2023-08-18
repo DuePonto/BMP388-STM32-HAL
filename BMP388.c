799bfd3e53e9def8708bbe34da0781781e395ab8
sam <SEMIRocketBoy@gmail.com>
2023-08-18 17:50:58 +0300

    feat: Initial commit for BMP388 in C implementation
    Rewritten library for BMP388, from C++ -> C language. Previous version
    can be found at https://github.com/DuePonto/BMP_388_STM32_HAL.

---
+++ BMP388.c
@@ -0,0 +1,475 @@
+/*
+ * BMP388.c
+ *
+ *  Created on: Aug 18, 2023
+ *      Author: sam
+ */
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "BMP388.h"
+#include "math.h"
+
+/* ----- PRIVATE FUNCTIONS PROTOTYPES ----- */
+HAL_StatusTypeDef	BMP388_SoftReset(BMP388_HandleTypeDef *bmp);
+HAL_StatusTypeDef	BMP388_GetCalibData(BMP388_HandleTypeDef *bmp);
+			float	BMP388_CompensateTemp(BMP388_HandleTypeDef *bmp, uint32_t raw_temp, float *temp);
+			float	BMP388_CompensatePress(BMP388_HandleTypeDef *bmp, float temp, uint32_t raw_press, float *press);
+HAL_StatusTypeDef	BMP388_ReadBytes(BMP388_HandleTypeDef *bmp, BMP388_regs reg_addr, uint8_t *buff, uint8_t len);
+HAL_StatusTypeDef	BMP388_WriteBytes(BMP388_HandleTypeDef *bmp, BMP388_regs reg_addr, uint8_t *buff, uint8_t len);
+
+
+
+
+/* ---------------------------- */
+/* ----- PUBLIC FUNCTIONS ----- */
+/* ---------------------------- */
+
+/*!
+ *  @brief Function to initiate BMP388 and get calibration data
+ *
+ *	@param[in] bmp			: Pointer to BMP388 structure
+ *
+ *  @return Status of execution
+ *  @retval = HAL_OK  		-> Success
+ *  @retval != HAL_OK	  	-> Failure Info
+ */
+HAL_StatusTypeDef BMP388_Init(BMP388_HandleTypeDef *bmp){
+	HAL_StatusTypeDef rslt;
+	uint8_t chip_id;
+
+	// Read CHIP_ID byte
+	rslt = BMP388_ReadBytes(bmp, CHIP_ID, &chip_id, 1);
+	if(rslt == HAL_OK && chip_id == BMP388_CHIP_ID){
+		// using softreset command
+		rslt = BMP388_SoftReset(bmp);
+		if(rslt == HAL_OK){
+			// get calibration data
+			rslt = BMP388_GetCalibData(bmp);
+		}
+		else{
+			return rslt;
+		}
+	}
+	else{
+		return rslt;
+	}
+
+	return rslt;
+}
+
+
+
+/*!
+ *  @brief Function to set temperature measurment oversampling
+ *
+ *	@param[in] bmp			: Pointer to BMP388 structure
+ *
+ *  @return Status of execution
+ *  @retval = HAL_OK  		-> Success
+ *  @retval != HAL_OK	  	-> Wrong oversampling mode
+ */
+HAL_StatusTypeDef BMP388_SetTempOS(BMP388_HandleTypeDef *bmp, uint8_t oversample){
+	if(oversample > BMP388_OVERSAMPLING_32X){
+		return HAL_ERROR;
+	}
+	bmp->_oversampling = (bmp->_oversampling & 0b11111000) | (oversample << 3);
+	return HAL_OK;
+}
+
+
+
+/*!
+ *  @brief Function to set pressure measurment oversampling
+ *
+ *	@param[in] bmp			: Pointer to BMP388 structure
+ *
+ *  @return Status of execution
+ *  @retval = HAL_OK  		-> Success
+ *  @retval != HAL_OK	  	-> Wrong oversampling mode
+ */
+HAL_StatusTypeDef BMP388_SetPressOS(BMP388_HandleTypeDef *bmp, uint8_t oversample){
+	if(oversample > BMP388_OVERSAMPLING_32X){
+		return HAL_ERROR;
+	}
+	bmp->_oversampling = (bmp->_oversampling & 0b11000111) | oversample;
+	return HAL_OK;
+}
+
+
+
+
+HAL_StatusTypeDef BMP388_SetIIRFilterCoeff(BMP388_HandleTypeDef *bmp, uint8_t filtercoeff){
+	if(filtercoeff > BMP3_IIR_FILTER_COEFF_127){
+		return HAL_ERROR;
+	}
+	bmp->_filtercoeff = filtercoeff;
+	return HAL_OK;
+}
+
+
+
+
+HAL_StatusTypeDef BMP388_SetOutputDataRate(BMP388_HandleTypeDef *bmp, uint8_t odr){
+	if(odr > BMP3_ODR_0_001_HZ){
+		return HAL_ERROR;
+	}
+	bmp->_odr = odr;
+	return HAL_OK;
+}
+
+
+
+/*!
+ *  @brief Function to read pressure and temperature from BMP388
+ *
+ *	@param[in]	bmp				: Pointer to BMP388 structure
+ *  @param[out] raw_pressure	: Pointer to the variable that contain uncompensated pressure data.
+ *	@param[out] raw_temperature	: Pointer to the variable that contain uncompensated temperature data.
+ *
+ *  @return Status of execution
+ *  @retval = HAL_OK  		-> Success
+ *  @retval != HAL_OK	  	-> Failure Info
+ */
+HAL_StatusTypeDef BMP388_ReadRawPressTemp(BMP388_HandleTypeDef *bmp, uint32_t *raw_pressure, uint32_t *raw_temperature){
+	HAL_StatusTypeDef rslt;
+	uint8_t power_mode = 0b00100011;
+
+	uint8_t oversampling = bmp->_oversampling;
+	uint8_t odr = bmp->_odr;
+	uint8_t filtercoeff = bmp->_filtercoeff;
+
+
+
+	// Set OSR register
+	rslt = BMP388_WriteBytes(bmp, OSR, &oversampling, 1);
+	if(rslt != HAL_OK){
+		return rslt;
+	}
+	// Set ODR register
+	rslt = BMP388_WriteBytes(bmp, ODR, &odr, 1);
+	if(rslt != HAL_OK){
+		return rslt;
+	}
+	// Set CONFIG register
+	rslt = BMP388_WriteBytes(bmp, CONFIG, &filtercoeff, 1);
+	if(rslt != HAL_OK){
+		return rslt;
+	}
+	// Set PWR_CTRL register
+	rslt = BMP388_WriteBytes(bmp, PWR_CTRL, &power_mode, 1);
+	if(rslt != HAL_OK){
+		return rslt;
+	}
+
+	uint8_t raw_data[6];
+	// Get raw data for pressure and temperature
+	rslt = BMP388_ReadBytes(bmp, DATA_0, raw_data, 6);
+	if(rslt != HAL_OK){
+		return rslt;
+	}
+	// Temporary variables to store the sensor data
+	uint32_t data_xlsb;
+	uint32_t data_lsb;
+	uint32_t data_msb;
+
+	// Parsing pressure data
+	data_xlsb = (uint32_t)raw_data[0];
+	data_lsb = (uint32_t)raw_data[1] << 8;
+	data_msb = (uint32_t)raw_data[2] << 16;
+	*raw_pressure = data_msb | data_lsb | data_xlsb;
+
+	// Parsing temperature data
+	data_xlsb = (uint32_t)raw_data[3];
+	data_lsb = (uint32_t)raw_data[4] << 8;
+	data_msb = (uint32_t)raw_data[5] << 16;
+	*raw_temperature = data_msb | data_lsb | data_xlsb;
+
+	return rslt;
+}
+
+
+
+/*!
+ *  @brief Function to compensate pressure and temperature from BMP388
+ *
+ *	@param[in]	bmp				: Pointer to BMP388 structure
+ *  @param[in] raw_pressure		: Variable that contain uncompensated pressure data.
+ *	@param[in] raw_temperature	: Variable that contain uncompensated temperature data.
+ *	@param[out] pressure		: Pointer to the variable that contain pressure.
+ *	@param[out]	temperature		: Pointer to the variable that contain temperature.
+ *
+ *  @return none
+ */
+void BMP388_CompensateRawPressTemp(BMP388_HandleTypeDef *bmp, uint32_t raw_pressure, uint32_t raw_temperature,
+									  	  	  	  	  	  	  float *pressure, float *temperature){
+	float temp;
+	float press;
+
+	BMP388_CompensateTemp(bmp, raw_temperature, &temp);
+	BMP388_CompensatePress(bmp, temp, raw_pressure, &press);
+
+	*pressure = press;
+	*temperature = temp;
+}
+
+
+
+/*!
+ *  @brief Function to find altutede value based on pressure measurment
+ *
+ *	@param[in] ground_pressure	: Pressure at ground
+ *  @param[in] pressure			: Pressure that measured at flight.
+ *
+ *  @return Altitude
+ */
+float BMP388_FindAltitude(float ground_pressure, float pressure){
+	  // Equation taken from BMP180 datasheet (page 16):
+	  //  http://www.adafruit.com/datasheets/BST-BMP180-DS000-09.pdf
+
+	  // Note that using the equation from wikipedia can give bad results
+	  // at high altitude. See this thread for more information:
+	  //  http://forums.adafruit.com/viewtopic.php?f=22&t=58064
+
+	  return 44330.0 * (1.0 - pow(pressure / ground_pressure, 0.1903));
+}
+
+
+
+
+
+/* ----------------------------- */
+/* ----- PRIVATE FUNCTIONS ----- */
+/* ----------------------------- */
+
+/*!
+ *  @brief Function to send softreset cmnd
+ *
+ *	@param[in] bmp			: Pointer to BMP388 structure
+ *
+ *  @return Status of execution
+ *  @retval = HAL_OK  		-> Success
+ *  @retval != HAL_OK	  	-> Failure Info
+ */
+HAL_StatusTypeDef BMP388_SoftReset(BMP388_HandleTypeDef *bmp){
+	uint8_t rst_cmnd = BMP388_SOFTRESET;
+    uint8_t cmd_rdy_status;
+    uint8_t cmd_err_status;
+
+	HAL_StatusTypeDef rslt;
+
+	// Reading STATUS reg to understand that the BMP388 is ready to receive command
+	rslt = BMP388_ReadBytes(bmp, STATUS, &cmd_rdy_status, 1);
+	if((rslt == HAL_OK) && (cmd_rdy_status & BMP388_CMD_RDY)){
+		// Writing SOFTRESET command to CMD reg
+		rslt = BMP388_WriteBytes(bmp, CMD, &rst_cmnd, 1);
+		if(rslt == HAL_OK){
+			// 2 ms pause then check ERR reg
+			HAL_Delay(2);
+			rslt = BMP388_ReadBytes(bmp, ERR_REG, &cmd_err_status, 1);
+			if((cmd_err_status & CMD) || (rslt != HAL_OK)){
+				return rslt;
+			}
+		}
+		else{
+			return rslt;
+		}
+	}
+
+	return rslt;
+}
+
+
+
+/*!
+ *  @brief Function to get calibration data
+ *
+ *	@param[in] bmp			: Pointer to BMP388 structure
+ *
+ *  @return Status of execution
+ *  @retval = HAL_OK  		-> Success
+ *  @retval != HAL_OK	  	-> Failure Info
+ */
+HAL_StatusTypeDef BMP388_GetCalibData(BMP388_HandleTypeDef *bmp){
+	HAL_StatusTypeDef rslt;
+	uint8_t calib_buff[BMP388_CALIBDATA_LEN] = {0};
+
+	uint16_t	raw_par_t1;
+	uint16_t	raw_par_t2;
+	int8_t		raw_par_t3;
+	int16_t		raw_par_p1;
+	int16_t		raw_par_p2;
+	int8_t		raw_par_p3;
+	int8_t		raw_par_p4;
+	uint16_t	raw_par_p5;
+	uint16_t	raw_par_p6;
+	int8_t		raw_par_p7;
+	int8_t		raw_par_p8;
+	int16_t		raw_par_p9;
+	int8_t		raw_par_p10;
+	int8_t		raw_par_p11;
+
+	rslt = BMP388_ReadBytes(bmp, CALIB_DATA, calib_buff, BMP388_CALIBDATA_LEN);
+
+	float temp_var;
+	if(rslt == HAL_OK){
+		// PAR_T1
+		temp_var = 0.00390625f;
+		raw_par_t1 = ((uint16_t)calib_buff[1] << 8) | (uint16_t)calib_buff[0];
+		bmp->_calib_data.par_t1 = (float)raw_par_t1 / temp_var;
+		// PAR_T2
+		temp_var = 1073741824.f;
+		raw_par_t2 = ((uint16_t)calib_buff[3] << 8) | (uint16_t)calib_buff[2];
+		bmp->_calib_data.par_t2 = (float)raw_par_t2 / temp_var;
+		// PAR_T3
+		temp_var = 281474976710656.f;
+		raw_par_t3 = calib_buff[4];
+		bmp->_calib_data.par_t3 = (float)raw_par_t3 / temp_var;
+		// PAR_P1
+		temp_var = 1048576.f;
+		raw_par_p1 = ((int16_t)calib_buff[6] << 8) | (int16_t)calib_buff[5];
+		bmp->_calib_data.par_p1 = ((float)raw_par_p1 - 16384) / temp_var;
+		// PAR_P2
+		temp_var = 536870912.f;
+		raw_par_p2 = ((int16_t)calib_buff[8] << 8) | (int16_t)calib_buff[7];
+		bmp->_calib_data.par_p2 = ((float)raw_par_p2 - 16384) / temp_var;
+		// PAR_P3
+		temp_var = 4294967296.f;
+		raw_par_p3 = (int8_t)calib_buff[9];
+		bmp->_calib_data.par_p3 = (float)raw_par_p3 / temp_var;
+		// PAR_P4
+		temp_var = 137438953472.f;
+		raw_par_p4 = (int8_t)calib_buff[10];
+		bmp->_calib_data.par_p4 = (float)raw_par_p4 / temp_var;
+		// PAR_P5
+		temp_var = 0.125f;
+		raw_par_p5 = ((uint16_t)calib_buff[12] << 8) | (uint16_t)calib_buff[11];
+		bmp->_calib_data.par_p5 = (float)raw_par_p5 / temp_var;
+		// PAR_P6
+		temp_var = 64.f;
+		raw_par_p6 = ((uint16_t)calib_buff[14] << 8) | (uint16_t)calib_buff[13];
+		bmp->_calib_data.par_p6 = (float)raw_par_p6 / temp_var;
+		// PAR_P7
+		temp_var = 256.f;
+		raw_par_p7 = (int8_t)calib_buff[15];
+		bmp->_calib_data.par_p7 = (float)raw_par_p7 / temp_var;
+		// PAR_P8
+		temp_var = 32768.f;
+		raw_par_p8 = (int8_t)calib_buff[16];
+		bmp->_calib_data.par_p8 = (float)raw_par_p8 / temp_var;
+		// PAR_P9
+		temp_var = 281474976710656.f;
+		raw_par_p9 = ((int16_t)calib_buff[18] << 8) | (int16_t)calib_buff[17];
+		bmp->_calib_data.par_p9 = (float)raw_par_p9 / temp_var;
+		// PAR_P10
+		temp_var = 281474976710656.f;
+		raw_par_p10 = (int8_t)calib_buff[19];
+		bmp->_calib_data.par_p10 = (float)raw_par_p10 / temp_var;
+		// PAR_P11
+		temp_var = 36893488147419103232.f;
+		raw_par_p11 = (int8_t)calib_buff[20];
+		bmp->_calib_data.par_p11 = (float)raw_par_p11 / temp_var;
+	}
+	return rslt;
+}
+
+
+
+/*!
+ *  @brief Function to compensate raw temperature data
+ *
+ *	@param[in] bmp			: Pointer to BMP388 structure
+ *	@parem[in] raw_temp		: Raw temperature data that need to be compensated
+ *	@param[out] temp		: Measured temp in celsius
+ *
+ *  @return Status of execution
+ *  @retval = HAL_OK  		-> Success
+ *  @retval != HAL_OK	  	-> Failure Info
+ */
+float BMP388_CompensateTemp(BMP388_HandleTypeDef *bmp, uint32_t raw_temp, float *temp){
+    float partial_data1 = (float)(raw_temp - bmp->_calib_data.par_t1);;
+    float partial_data2 = (float)(partial_data1 * bmp->_calib_data.par_t2);
+
+    *temp = partial_data2 + (partial_data1 * partial_data1) * bmp->_calib_data.par_t3;
+
+    return *temp;
+}
+
+
+
+/*!
+ *  @brief Function to compensate raw pressure data
+ *
+ *	@param[in] bmp			: Pointer to BMP388 structure
+ *	@param[in] temp			: Temperature that assosiated with pressure measurment
+ *	@param[in] raw_press	: Raw pressure data that need to be compensated
+ *	@param[out] press		: Measured pressure in Pa
+ *
+ *  @return Status of execution
+ *  @retval = press			: Compensated pressure value
+ */
+float BMP388_CompensatePress(BMP388_HandleTypeDef *bmp, float temp, uint32_t raw_press, float *press){
+    float partial_data1;
+    float partial_data2;
+    float partial_data3;
+    float partial_data4;
+    float partial_out1;
+    float partial_out2;
+
+
+
+    partial_data1 = bmp->_calib_data.par_p6 * temp;
+    partial_data2 = bmp->_calib_data.par_p7 * (temp * temp);
+    partial_data3 = bmp->_calib_data.par_p8 * (temp * temp * temp);
+    partial_out1 = bmp->_calib_data.par_p5 + partial_data1 + partial_data2 + partial_data3;
+
+    partial_data1 = bmp->_calib_data.par_p2 * temp;
+    partial_data2 = bmp->_calib_data.par_p3 * (temp * temp);
+    partial_data3 = bmp->_calib_data.par_p4 * (temp * temp * temp);
+    partial_out2 = (float)raw_press * (bmp->_calib_data.par_p1 + partial_data1 + partial_data2 + partial_data3);
+
+    partial_data1 = (float)raw_press * (float)raw_press;
+    partial_data2 = bmp->_calib_data.par_p9 + bmp->_calib_data.par_p10 * temp;
+    partial_data3 = partial_data1 * partial_data2;
+    partial_data4 = partial_data3 + ((float)raw_press * (float)raw_press * (float)raw_press) * bmp->_calib_data.par_p11;
+
+    *press = partial_out1 + partial_out2 + partial_data4;
+
+    return *press;
+}
+
+
+
+/*!
+ *  @brief Function to read byte from BMP388 in blocking mode
+ *
+ *	@param[in] bmp			: Pointer to BMP388 structure
+ *  @param[in] reg_addr     : Register address.
+ *  @param[out] buff	    : Pointer to the data buffer to store the read data.
+ *  @param[in] len          : Amount of bytes to read.
+ *
+ *  @return Status of execution
+ *  @retval = HAL_OK 		-> Success
+ *  @retval != HAL_ERROR 	-> Failure Info
+ */
+HAL_StatusTypeDef BMP388_ReadBytes(BMP388_HandleTypeDef *bmp, BMP388_regs reg_addr, uint8_t *buff, uint8_t len){
+	return HAL_I2C_Mem_Read(bmp->_hi2c, BMP388_ADDR << 1, reg_addr, I2C_MEMADD_SIZE_8BIT, buff, len, 100);
+}
+
+
+
+/*!
+ *  @brief Function to write byte from BMP388 in blocking mode
+ *
+ *	@param[in] bmp			: Pointer to BMP388 structure
+ *  @param[in] reg_addr     : Register address.
+ *  @param[out] buff	    : Pointer to the data buffer to store the read data.
+ *  @param[in] len          : Amount of bytes to write.
+ *
+ *  @return Status of execution
+ *  @retval = HAL_OK 		-> Success
+ *  @retval != HAL_ERROR 	-> Failure Info
+ */
+HAL_StatusTypeDef BMP388_WriteBytes(BMP388_HandleTypeDef *bmp, BMP388_regs reg_addr, uint8_t *buff, uint8_t len){
+	return HAL_I2C_Mem_Write(bmp->_hi2c, BMP388_ADDR << 1, reg_addr, I2C_MEMADD_SIZE_8BIT, buff, len, 100);
+}
---
+++ BMP388.h
@@ -0,0 +1,145 @@
+/*
+ * BMP388.h
+ *
+ *  Created on: Aug 18, 2023
+ *      Author: sam
+ */
+
+#ifndef LIB_BMP388_HAL_BMP388_H_
+#define LIB_BMP388_HAL_BMP388_H_
+
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l4xx_hal.h"
+
+
+/* Declarations and definitions ----------------------------------------------*/
+#define BMP388_ADDR 				0x76
+
+#define BMP388_CHIP_ID				0x50
+
+// BMP 388 commands
+#define BMP388_CMD_RDY				0x10
+#define BMP388_SOFTRESET			0xB6
+
+// Over sampling macros
+#define BMP388_NO_OVERSAMPLING		0b00000000
+#define BMP388_OVERSAMPLING_2X      0b00000001
+#define BMP388_OVERSAMPLING_4X      0b00000010
+#define BMP388_OVERSAMPLING_8X      0b00000011
+#define BMP388_OVERSAMPLING_16X     0b00000100
+#define BMP388_OVERSAMPLING_32X     0b00000101
+
+// Filter setting macros
+#define BMP3_IIR_FILTER_DISABLE     0b00000000
+#define BMP3_IIR_FILTER_COEFF_1     0b00000001
+#define BMP3_IIR_FILTER_COEFF_3     0b00000010
+#define BMP3_IIR_FILTER_COEFF_7     0b00000011
+#define BMP3_IIR_FILTER_COEFF_15    0b00000100
+#define BMP3_IIR_FILTER_COEFF_31    0b00000101
+#define BMP3_IIR_FILTER_COEFF_63    0b00000110
+#define BMP3_IIR_FILTER_COEFF_127   0b00000111
+
+// output data rate macros
+#define BMP3_ODR_200_HZ             0b00000000
+#define BMP3_ODR_100_HZ             0b00000001
+#define BMP3_ODR_50_HZ              0b00000010
+#define BMP3_ODR_25_HZ              0b00000011
+#define BMP3_ODR_12_5_HZ            0b00000100
+#define BMP3_ODR_6_25_HZ            0b00000101
+#define BMP3_ODR_3_1_HZ             0b00000110
+#define BMP3_ODR_1_5_HZ             0b00000111
+#define BMP3_ODR_0_78_HZ            0b00001000
+#define BMP3_ODR_0_39_HZ            0b00001001
+#define BMP3_ODR_0_2_HZ             0b00001010
+#define BMP3_ODR_0_1_HZ             0b00001011
+#define BMP3_ODR_0_05_HZ            0b00001100
+#define BMP3_ODR_0_02_HZ            0b00001101
+#define BMP3_ODR_0_01_HZ            0b00001110
+#define BMP3_ODR_0_006_HZ           0b00001111
+#define BMP3_ODR_0_003_HZ           0b00010000
+#define BMP3_ODR_0_001_HZ           0b00010001
+
+#define BMP388_CALIBDATA_LEN	21
+
+
+/* BMP388 registers */
+typedef enum{
+	CHIP_ID					= 0x00,
+	ERR_REG					= 0x02,
+	STATUS					= 0x03,
+	DATA_0					= 0x04, // PRESS_XLSB_7_0
+	DATA_1					= 0x05, // PRESS_LSB_15_8
+	DATA_2			 		= 0x06, // PRESS_MSB_23_16
+	DATA_3					= 0x07, // TEMP_XLSB_7_0
+	DATA_4					= 0x08, // TEMP_LSB_15_8
+	DATA_5					= 0x09, // TEMP_MSB_23_16
+	SENSORTIME_0			= 0x0C,
+	SENSORTIME_1			= 0x0D,
+	SENSORTIME_2			= 0x0E,
+	EVENT					= 0x10,
+	INT_STATUS				= 0x11,
+	FIFO_LENGTH_0			= 0x12,
+	FIFO_LENGTH_1			= 0x13,
+	FIFO_DATA				= 0x14,
+	FIFO_WTM_0				= 0x15,
+	FIFO_WTM_1				= 0x16,
+	FIFO_CONFIG_1			= 0x17,
+	FIFO_CONFIG_2			= 0x18,
+	INT_CTRL				= 0x19,
+	IF_CONF					= 0x1A,
+	PWR_CTRL				= 0x1B,
+	OSR						= 0x1C,
+	ODR						= 0x1D,
+	CONFIG					= 0x1F,
+	CALIB_DATA				= 0x31,
+	CMD						= 0x7E,
+}BMP388_regs;
+
+/* Structure for calibration parameters */
+typedef struct{
+	float		par_t1;
+	float		par_t2;
+	float		par_t3;
+	float		par_p1;
+	float		par_p2;
+	float		par_p3;
+	float		par_p4;
+	float		par_p5;
+	float		par_p6;
+	float		par_p7;
+	float		par_p8;
+	float		par_p9;
+	float		par_p10;
+	float		par_p11;
+}Calib_data;
+
+
+
+
+/* BMP388 structure -----------------------------------------------------------------*/
+typedef struct{
+	I2C_HandleTypeDef 		*_hi2c;
+
+	uint8_t					_oversampling;
+	uint8_t					_filtercoeff;
+	uint8_t					_odr;
+
+	Calib_data				_calib_data;
+}BMP388_HandleTypeDef;
+
+
+/* Public functions -----------------------------------------------------------------*/
+HAL_StatusTypeDef	BMP388_Init(BMP388_HandleTypeDef *bmp);
+HAL_StatusTypeDef	BMP388_SetTempOS(BMP388_HandleTypeDef *bmp, uint8_t oversample);
+HAL_StatusTypeDef	BMP388_SetPressOS(BMP388_HandleTypeDef *bmp, uint8_t oversample);
+HAL_StatusTypeDef 	BMP388_SetIIRFilterCoeff(BMP388_HandleTypeDef *bmp, uint8_t filtercoeff);
+HAL_StatusTypeDef	BMP388_SetOutputDataRate(BMP388_HandleTypeDef *bmp, uint8_t odr);
+HAL_StatusTypeDef	BMP388_ReadRawPressTemp(BMP388_HandleTypeDef *bmp, uint32_t *raw_pressure, uint32_t *raw_temperature);
+			 void	BMP388_CompensateRawPressTemp(BMP388_HandleTypeDef *bmp, uint32_t raw_pressure, uint32_t raw_temperature,
+					 	 	 	 	 	 	 	 	 	 	 	 	 	     float *pressure, float *temperature);
+			float	BMP388_FindAltitude(float ground_pressure, float pressure);
+
+
+
+#endif /* LIB_BMP388_HAL_BMP388_H_ */
